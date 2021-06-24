c38d4ff56f8f99256b65b83eb5c15eb18fc17838
ERICK LUZ CAVALCANTE <erickl.cavalcante@gmail.com>
2021-06-24 18:57:37 -0300

    modificado o valor da variavel numeroDeVezes

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace aplicacaoConsoleDotNetCore
     {
         static void Main(string[] args)
         {
-            int numeroDeVezes = 5;
+            int numeroDeVezes = 10;
             for (int i=0; i< numeroDeVezes; i++){
                 Console.WriteLine($"Bem vindo ao curso de .NET {i}");
             }
